b09f7c16d791e8404ecfabb0718fd7dc18a59eff
Jeffrey Su <sogajeffrey@gmail.com>
2021-09-02 13:24:54 -0700

    make cron optional

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Simple directory backup with RClone
 ## Environment Variables:
 | Variable | Required? | Default | Description |
 | -------- |:--------- |:------- |:----------- |
-| `CRON` | Required |  | CRON schedule for backup |
+| `CRON` | Optional |  | CRON schedule for backup |
 | `BACKUP_DIR` | Required |  | Full path to directory to backup |
 | `FILE_PREFIX` | Required |  | Directory to backup |
 | `DELETE_OLDER_THAN` | Optional | `None` | Optionally, delete files older than `DELETE_OLDER_THAN` (s,m,d,y). Eg: 1d - one day |
--- init.sh
+++ init.sh
@@ -2,18 +2,22 @@
 
 set -e
 
-if [[ -z "$CRON" ]]; then
-    echo "Please set the cron schedule via CRON env var"
-	exit 1
+if [[ ! -z "$CRON" ]]; then
+    echo "CRON enabled"
+    echo "${CRON} /backup.sh" > /etc/cron.d/backup-cron
+    echo "### EMPTY LINE FOR CRON" >> /etc/cron.d/backup-cron
+
+    chmod 0644 /etc/cron.d/backup-cron
+
+    cron
+
+    echo "Backup cron container started with schedule ${CRON}" > /var/log/cron.log
+
+    tail -f /var/log/cron.log
 fi
 
-echo "${CRON} /backup.sh" > /etc/cron.d/backup-cron
-echo "### EMPTY LINE FOR CRON" >> /etc/cron.d/backup-cron
+if [[ -z "$CRON" ]]; then
+    echo "One time run of backup"
+    /backup.sh
+fi
 
-chmod 0644 /etc/cron.d/backup-cron
-
-cron
-
-echo "Backup cron container started with schedule ${CRON}" > /var/log/cron.log
-
-tail -f /var/log/cron.log
